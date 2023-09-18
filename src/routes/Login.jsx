22263de35fcd3e9cb0c17d9c9c3140119635e351
nwy98 <97599088+norunaru@users.noreply.github.com>
2023-09-18 17:13:00 +0900

    Login page completed

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,6 +3,97 @@ import axios from "axios";
 import { useSetRecoilState } from "recoil";
 import { isLoggedInState } from "../atoms";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Page = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  /* 전체 화면 백그라운드 스타일 설정 */
+  background-size: 100% 100%;
+  background-position: 0px 0px, 0px 0px, 0px 0px, 0px 0px, 0px 0px, 0px 0px,
+    0px 0px, 0px 0px;
+  background-image: radial-gradient(
+      70% 53% at 36% 76%,
+      #000000f5 0%,
+      #073aff00 100%
+    ),
+    radial-gradient(42% 53% at 15% 94%, #000000f5 7%, #073aff00 100%),
+    radial-gradient(42% 53% at 34% 72%, #903df4f5 7%, #073aff00 100%),
+    radial-gradient(18% 28% at 35% 87%, #000000f5 7%, #073aff00 100%),
+    radial-gradient(31% 43% at 7% 98%, #0f0f17f5 24%, #073aff00 100%),
+    radial-gradient(35% 56% at 91% 74%, #0e0c55f5 9%, #073aff00 100%),
+    radial-gradient(74% 86% at 67% 38%, #000000f5 24%, #073aff00 100%),
+    linear-gradient(181deg, #085877ff 1%, #4c00fcff 100%);
+`;
+
+const Container = styled.div`
+  padding: 20px 30px;
+  border-radius: 15px;
+  background-color: white;
+  height: 600px;
+  width: 500px;
+`;
+
+const Header = styled.header`
+  margin: 90px 0px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    color: black;
+    margin-bottom: 20px;
+    font-size: 30px;
+    font-weight: 600;
+  }
+  p {
+    font-size: 16px;
+    color: black;
+    width: 60%;
+    opacity: 0.5;
+  }
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 0px 30px;
+  a {
+    font-size: 18px;
+    display: block;
+    text-align: center;
+    text-decoration: none;
+    padding: 10px 0px;
+    background-color: #9b75f3;
+    color: white;
+    border-radius: 5px;
+  }
+`;
+
+const Input = styled.input`
+  border: none;
+  padding: 15px 0px;
+  font-size: 18px;
+  margin-bottom: 25px;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+  color: rgba(0, 0, 0, 0.4);
+  transition: border-color 0.3s ease-in-out;
+
+  &:hover {
+    border-color: ${(props) => props.theme.accentColor};
+  }
+
+  &[type="submit"] {
+    background-color: ${(props) => props.theme.accentColor};
+    cursor: pointer;
+    padding: 20px 0px;
+    border-radius: 5px;
+    color: white;
+  }
+`;
 
 const Login = () => {
   const navigate = useNavigate();
@@ -14,7 +105,12 @@ const Login = () => {
   // isLoggedInState의 값을 업데이트하기 위한 Recoil hook
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Email and Password are required.");
+      return; // Exit the function early if fields are empty
+    }
     //로그인 성공 부분 -> 성공시 반환된 토큰을 로컬 스토리지에 저장, onLogin(true)호출하여 부모 컴포넌트에 로그인 상태를 알림.
     try {
       //서버로 email, password보내고 서버에서 응답 객체가 오는데 그것을 response변수에 넣는다.
@@ -40,24 +136,35 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <Page>
+      <Container>
+        <Header>
+          <h1>Welcome</h1>
+          <p>Log in or create Account</p>
+        </Header>
+        <LoginForm onSubmit={handleLogin}>
+          <Input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Input type="submit" value="Log In" />
+          {error && <p style={{ color: "red", marginLeft: "1rem" }}>{error}</p>}
+          <Link to="/signup">Create Account</Link>
+        </LoginForm>
+        {/* <button onClick={handleLogin}>Login</button>
       {error && <p style={{ color: "red" }}>{error}</p>}{" "}
-      {/* 에러 메시지 표시 */}
-    </div>
+      에러 메시지 표시 */}
+      </Container>
+    </Page>
   );
 };
 
