1bb349e84878dbc88760c2000f1246178ec659de
nwy98 <97599088+norunaru@users.noreply.github.com>
2023-11-01 21:36:12 +0900

    -완-

--- src/atoms.js
+++ src/atoms.js
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 export const isLoggedInState = atom({
   key: "isLoggedInState",
-  default: true, // 초기 상태: 로그아웃 상태(false)
+  default: false, // 초기 상태: 로그아웃 상태(false)
 });
 
 export const userNicknameAtom = atom({
